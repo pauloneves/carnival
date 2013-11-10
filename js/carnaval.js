10005d2397c6563f56b31abacf171a391a4ccc32
Paulo Eduardo Neves <pauloneves@gmail.com>
2013-11-10 19:35:38 -0200

    show! clica no coração e aparece na lista do itinerário próprio. Mensagens para compartilhar.

--- js/carnaval.js
+++ js/carnaval.js
@@ -1,16 +1,17 @@
-var SC = {}
+var SC = {};
 
 SC.getParadesHTML = function(parades) {
     var all = [];
     for (var day=0 , length=parades.length; day<length; day++) {
-        var diaHtml = '<div class="col-6 col-sm-6 col-lg-4">' +
+        var diaHtml = '<div class="col-6 col-sm-6 col-lg-4 dia_desfile">' +
                       '<h3>' +  parades[day][0].dia_title + '</h3><p><ul>';
         var atLeastOne = 0;
         for (var i= 0, dayLength=parades[day].length; i<dayLength; i++) {
             var parade = parades[day][i];
             if (parade.bairro === 'Centro' || parade.bairro === 'Lapa') {
                 diaHtml += '<li>' +  parade.nome +  ', ' + parade.hora.substring(0,2)
-                           + 'h <span class="glyphicon glyphicon-heart-empty"></span></li>';
+                           + 'h <span class="glyphicon glyphicon-heart-empty favorite" id="' + parade.nome_url
+                           + '"></span></li>';
                 atLeastOne++;
             }
 
@@ -21,7 +22,7 @@ SC.getParadesHTML = function(parades) {
         }
     }
     return all;
-}
+};
 
 SC.displayParades = function() {
     $.ajax('js/desfiles.json').done(
@@ -29,4 +30,25 @@ SC.displayParades = function() {
             var all =  SC.getParadesHTML(json);
             $("#carnival_parades" ).html(all.join(''));
         });
-}
+};
+
+SC.changeFavorite = function() {
+    $(this).toggleClass('glyphicon-heart-empty');
+    $(this).toggleClass('glyphicon-heart');
+};
+
+SC.copyFavorites = function () {
+    var favs = $('#parades .dia_desfile').has('.glyphicon-heart').clone();
+    var favList = $('#favorite_parades');
+    favList.children().remove();
+    if (favs.length) {
+        favs.find('li').has('.glyphicon-heart-empty').remove();
+        $('#itinerary-message').hide();
+        favList.append(favs);
+        $('#itinerary-share-message').show();
+    } else {
+        $('#itinerary-message').show();
+        $('#itinerary-share-message').hide();
+    }
+};
+
